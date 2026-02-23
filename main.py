dc91a1b9aec6c84ab76f35ff7205acdbb1eea340
yfree1232-afk <yfree1232@gmail.com>
2026-02-23 17:59:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -113,3 +113,21 @@ f"""
 """)
 
 bot.infinity_polling()
+
+from flask import Flask
+import threading
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot running"
+
+def run():
+    app.run(host='0.0.0.0', port=10000)
+
+def keep_alive():
+    t = threading.Thread(target=run)
+    t.start()
+
+keep_alive()
